81f3e1ed1949c1d4173e10d57a5dd48143fee412
Fletcher Dick <fdi19@uclive.ac.nz>
2021-09-13 12:08:37 +1200

    Seperated the main and the headline into components

---
+++ src/components/Headline.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="headline">{{this.text}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Headline',
+  props: {
+      text: {
+          type: String,
+          required: true
+      }
+  }
+}
+</script>
+
+<style scoped>
+.headline {
+    font-family: "NunitoSans-Black",sans-serif;
+    background-image: linear-gradient(
+60deg,rgba(239,0,143,.5),rgba(110,195,244,.5),rgba(112,56,255,.5),rgba(255,186,39,.5));
+    background-size: 400% 100%;
+    -webkit-background-clip: text;
+    background-clip: none;
+    -webkit-text-fill-color: transparent;
+    animation: hue 5s infinite alternate;
+    line-height: 1.1;
+    font-size: 18ex;
+}
+
+@keyframes hue {
+  0% {
+    filter: hue-rotate(0deg);
+  }
+  
+
+  100% {
+    filter: hue-rotate(-1turn);
+  }
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Introduction section -->
+  <div>
+    <img src="../assets/memoji_3.png" width="300">
+    <headline text="Fletcher Dick" />
+    <div>📍 Christchurch, New Zealand</div>
+    <div>Welcome to my portfolio! There's going to be a lot changing around here 😮</div>
+  </div>
+</template>
+
+<script>
+import Headline from '../components/Headline.vue'
+
+export default {
+  name: 'MainView',
+  components: {
+    Headline
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
