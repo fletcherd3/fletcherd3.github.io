9700ba098533114d37c3295e5734a4407b474009
Fletcher Dick <fdi19@uclive.ac.nz>
2021-09-13 18:49:23 +1200

    Resized and centered title text

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,38 +1,37 @@
 <template>
-  <div class="headline">{{this.text}}</div>
+  <div class="name">{{ this.text }}</div>
 </template>
 
 <script>
 export default {
   name: 'Headline',
   props: {
-      text: {
-          type: String,
-          required: true
-      }
+    text: {
+      type: String,
+      required: true
+    }
   }
 }
 </script>
 
-<style scoped>
-.headline {
-    font-family: "NunitoSans-Black",sans-serif;
-    background-image: linear-gradient(
-60deg,rgba(239,0,143,.5),rgba(110,195,244,.5),rgba(112,56,255,.5),rgba(255,186,39,.5));
-    background-size: 400% 100%;
-    -webkit-background-clip: text;
-    background-clip: none;
-    -webkit-text-fill-color: transparent;
-    animation: hue 5s infinite alternate;
-    line-height: 1.1;
-    font-size: 18ex;
+<style>
+.name {
+  background-image: linear-gradient(
+      60deg, rgba(239, 0, 143, .5), rgba(110, 195, 244, .5), rgba(112, 56, 255, .5), rgba(255, 186, 39, .5));
+  background-size: 400% 100%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-align: center;
+  animation: hue 5s infinite alternate;
+  line-height: 1.1;
+  font-size: 5rem;
 }
 
 @keyframes hue {
   0% {
     filter: hue-rotate(0deg);
   }
-  
+
 
   100% {
     filter: hue-rotate(-1turn);
