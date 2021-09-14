89399670c262463dde3ff60d2b3bb284eb8d08b3
Fletcher Dick <fdi19@uclive.ac.nz>
2021-09-15 08:41:16 +1200

    Added low res lazy image

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -8,7 +8,10 @@
     >
       <v-row>
         <v-col justify="center">
-          <v-img alt="An image of my Apple Memoji" class="mx-auto" src="../assets/memoji_2.png"
+          <v-img alt="An image of my Apple Memoji"
+                 class="mx-auto"
+                 lazy-src="../assets/memoji_0.5x.png"
+                 src="../assets/memoji_1x.png"
                  width="250px"/>
         </v-col>
       </v-row>
@@ -27,13 +30,19 @@
             href="https://drive.google.com/uc?export=download&id=1qUQ--AQqD0bHSLeDrIdRflzsyAQ0IyO5"
             outlined
         >
+          <v-icon
+              class="pr-5"
+              dark
+              right
+          >
+            mdi-download
+          </v-icon>
           Download CV
         </v-btn>
       </v-row>
     </v-container>
 
-
-    <v-container class="pt-7" color="black">
+    <v-container class="py-7">
       <v-row justify="center">
         <h4>Quick Links</h4>
       </v-row>
@@ -41,8 +50,8 @@
         <v-col
             v-for="(link, index) in links" :key="index"
             align="center"
-            justify="center"
             cols="12"
+            justify="center"
             sm="4"
         >
           <a :href="link.href" target="_blank">
@@ -53,6 +62,166 @@
         </v-col>
 
       </v-row>
+    </v-container>
+
+    <v-container class="pt-7 elevation-8" color="black">
+      <v-card
+          :loading="loading"
+          class="mx-auto my-12"
+          max-width="374"
+      >
+        <template slot="progress">
+          <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row
+              align="center"
+              class="mx-0"
+          >
+            <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">
+              4.5 (413)
+            </div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">
+            $ • Italian, Cafe
+          </div>
+
+          <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus
+            patio seating.
+          </div>
+        </v-card-text>
+
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-text>
+          <v-chip-group
+              v-model="selection"
+              active-class="deep-purple accent-4 white--text"
+              column
+          >
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+              color="deep-purple lighten-2"
+              text
+              @click="reserve"
+          >
+            Reserve
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card
+          :loading="loading"
+          class="mx-auto my-12"
+          max-width="374"
+      >
+        <template slot="progress">
+          <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+        <v-card-title>Cafe Badilico</v-card-title>
+
+        <v-card-text>
+          <v-row
+              align="center"
+              class="mx-0"
+          >
+            <v-rating
+                :value="4.5"
+                color="amber"
+                dense
+                half-increments
+                readonly
+                size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">
+              4.5 (413)
+            </div>
+          </v-row>
+
+          <div class="my-4 text-subtitle-1">
+            $ • Italian, Cafe
+          </div>
+
+          <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus
+            patio seating.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-text>
+          <v-chip-group
+              v-model="selection"
+              active-class="deep-purple accent-4 white--text"
+              column
+          >
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+              color="deep-purple lighten-2"
+              text
+              @click="reserve"
+          >
+            Reserve
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-container>
   </v-container>
 </template>
@@ -68,7 +237,7 @@ export default {
   data() {
     let email = 'fletcherjdick@gmail.com';
     return {
-      iconColour: 'Black',
+      iconColour: 'indigo',
       links: [
         {
           iconClass: 'fas fa-paper-plane',
