69de5c43ae2fe1f217e01f6b1827024a45fb80a1
Fletcher Dick <fdi19@uclive.ac.nz>
2021-09-14 15:58:36 +1200

    Simplified introduction section by removing redundant components

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,69 +1,72 @@
 <template>
   <!-- Introduction section -->
-  <v-container class="root">
-    <v-container class="elevation-16 pa-10">
-      <v-row
-          align="center"
-          fluid
-          justify="center"
-          style="height: 100vh;"
-
-      >
-        <div class="pb-16 mt-n16">
-          <v-row>
-            <v-col justify="center">
-              <v-img alt="An image of my Apple Memoji" class="mx-auto" src="../assets/memoji_2.png"
-                     width="250px"/>
-            </v-col>
-          </v-row>
-          <v-row class="mb-10" justify="center">
-            <headline text="Fletcher Dick"/>
-          </v-row>
-          <v-row justify="center">📍 Christchurch, New Zealand</v-row>
-          <v-row class="text-center" justify="center">
-            Welcome to my portfolio! There's going to be a lot changing around here 😮
-          </v-row>
-          <v-row class="mt-16" justify="center">
-            <v-btn
-                class="ma-2"
-                color="indigo"
-                href="https://drive.google.com/file/d/1qUQ--AQqD0bHSLeDrIdRflzsyAQ0IyO5/view?usp=sharing"
-                outlined
-                target="_blank"
-            >
-              My Resume
-            </v-btn>
-          </v-row>
-        </div>
+  <v-container class="pa-0">
+    <v-container
+        class="elevation-8 pa-10"
+        justify="center"
+        style="height: auto; min-height: 98vh;"
+    >
+      <v-row>
+        <v-col justify="center">
+          <v-img alt="An image of my Apple Memoji" class="mx-auto" src="../assets/memoji_2.png"
+                 width="250px"/>
+        </v-col>
+      </v-row>
+      <v-row class="mb-10" justify="center">
+        <headline text="Fletcher Dick"/>
+      </v-row>
+      <v-row justify="center">📍 Christchurch, New Zealand</v-row>
+      <v-row class="text-center" justify="center">
+        Welcome to my portfolio! There's going to be a lot changing around here 😮
+      </v-row>
+      <v-row class="mt-16" justify="center">
+        <v-btn
+            class="ma-2"
+            color="indigo"
+            href="https://drive.google.com/file/d/1qUQ--AQqD0bHSLeDrIdRflzsyAQ0IyO5/view?usp=sharing"
+            outlined
+            target="_blank"
+        >
+          My Resume
+        </v-btn>
       </v-row>
     </v-container>
 
+
     <v-container class="pt-7">
       <v-row justify="center">
-        Quick Links hi
+        <h4>Quick Links</h4>
       </v-row>
       <v-row>
         <v-col
             align="center"
             cols="12"
-            justify="center" sm="4">
+            sm="4"
+        >
+
+          <a :href="'mailto:'+email">
+            <em class="fab fa-linkedin fa-2x" v-bind:style="{ color: iconColour }"/>
+            {{ email }}
+          </a>
+        </v-col>
+        <v-col
+            align="center"
+            cols="12"
+            justify="center"
+            sm="4"
+        >
           <em class="fab fa-linkedin fa-2x" v-bind:style="{ color: iconColour }"/>
         </v-col>
         <v-col
             align="center"
             cols="12"
-            justify="center" sm="4">
-          <em class="fab fa-linkedin fa-2x" v-bind:style="{ color: iconColour }"/>
-        </v-col>
-        <v-col
-            align="center"
-            cols="12"
-            justify="center" sm="4">
+            justify="center"
+            sm="4"
+        >
           <em class="fab fa-linkedin fa-2x" v-bind:style="{ color: iconColour }"/>
         </v-col>
       </v-row>
     </v-container>
-
   </v-container>
 </template>
 
@@ -77,18 +80,14 @@ export default {
   },
   data() {
     return {
-      iconColour: 'Tomato'
+      iconColour: 'Tomato',
+      email: 'fletcherjdick@gmail.com'
     }
   }
 }
 </script>
 
 <style scoped>
-.root {
-  padding: 0 0 100px;
-  width: 100% !important;
-}
-
 .container {
   max-width: 100% !important;
 }
